abff92d0bef814bcf55ce837a8d4d1013d9af366
hamzanaeem03 <hamza04032003naeem@gmail.com>
2025-01-05 18:34:26 +0500

    added gitingnore and script

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+import express from "express"
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req , res)=>{
+console.log('first api')
+res.send('this is response')
+})
+
+
+app.listen(PORT , ()=> console.log('port is running'))
