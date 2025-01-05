4430a6e24b6cb9a4da64983497c7d3d0524677f8
hamzanaeem03 <hamza04032003naeem@gmail.com>
2025-01-05 18:54:17 +0500

    GET api for task aray

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const tasks = [
+    { id: 1, task: "Complete React project" },
+    { id: 2, task: "Study for Graph Theory exam" },
+    { id: 3, task: "Write blog post on Docker" },
+    { id: 4, task: "Finalize donation website design" },
+    { id: 5, task: "Prepare resume for job fair" }
+  ];
+
 import express from "express"
 const app = express();
 const PORT = 4000;
 
 app.get('/', (req , res)=>{
-console.log('first api')
-res.send('this is response')
+    res.status(200).send(tasks);
+// console.log('first api')
+// res.send('this is response')
 })
 
 
