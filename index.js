aff2ead62a240acca6a022589b6614d836ba6d28
hamzanaeem03 <hamza04032003naeem@gmail.com>
2025-01-06 20:41:05 +0500

    formatted code

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 const tasks = [
-    { id: 1, task: "Complete React project" },
-    { id: 2, task: "Study for Graph Theory exam" },
-    { id: 3, task: "Write blog post on Docker" },
-    { id: 4, task: "Finalize donation website design" },
-    { id: 5, task: "Prepare resume for job fair" }
-  ];
+  { id: 1, task: "Complete React project" },
+  { id: 2, task: "Study for Graph Theory exam" },
+  { id: 3, task: "Write blog post on Docker" },
+  { id: 4, task: "Finalize donation website design" },
+  { id: 5, task: "Prepare resume for job fair" },
+];
 
-import express from "express"
+import express from "express";
 const app = express();
 const PORT = 4000;
 
-app.get('/', (req , res)=>{
-    res.status(200).send(tasks);
-// console.log('first api')
-// res.send('this is response')
-})
+app.get("/", (req, res) => {
+  res.status(200).send(tasks);
+  // console.log('first api')
+  // res.send('this is response')
+});
 
+app.post("/", (req, res) => {
+  console.log("first api");
+  res.send("this is response from post request");
+});
 
-app.post('/', (req , res)=>{
-    console.log('first api')
-    res.send('this is response from post request')
-    })
+app.put("/", (req, res) => {
+  console.log("first api");
+  res.send("this is response from put request");
+});
 
+app.delete("/", (req, res) => {
+  console.log("first api");
+  res.send("this is response from delete request");
+});
 
-
-    app.put('/', (req , res)=>{
-        console.log('first api')
-        res.send('this is response from put request')
-        })
-
-        app.delete('/', (req , res)=>{
-            console.log('first api')
-            res.send('this is response from delete request')
-            })
-    
-app.listen(PORT , ()=> console.log('port is running'))
+app.listen(PORT, () => console.log("port is running"));
